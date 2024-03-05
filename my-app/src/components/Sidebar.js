aed5d929bc2398661154ac2e3286267c08d5b16a
otherworldly94b <and.charalambous@gmail.com>
2024-03-05 22:09:54 +0000

    challenge part 1

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+    return <div>
+        <a>MyPhotos</a>
+        <a>My Illustations</a>
+        <a>My Paintings</a>
+    </div>;
+}
+
+export default Sidebar;
