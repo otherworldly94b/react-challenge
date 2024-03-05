3188b7e3ba350d46de4d2b22931b6e987deca62e
otherworldly94b <and.charalambous@gmail.com>
2024-03-05 22:15:20 +0000

    challenge part 2 - css

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react'
+import css from "./css/Sidebar.module.css"
 
-const Sidebar = () => {
-    return <div>
-        <a>MyPhotos</a>
-        <a>My Illustations</a>
-        <a>My Paintings</a>
-    </div>;
+function Sidebar() {
+    return (
+        <div className = {css.sidebar}>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+    )
 }
 
-export default Sidebar;
+export default Sidebar
